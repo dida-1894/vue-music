dd6594d418f9305d9241fe801c87b289025e8011
alison <314256325@qq.com>
2018-11-02 20:44:53 +0800

    changing the scroll way and add a wather

--- src/components/rank/SongListDetail.vue
+++ src/components/rank/SongListDetail.vue
@@ -10,41 +10,41 @@
     </detail-header>
     <div class="bg">
     </div>
-    <img v-lazy="coverImgUrl" class="bg-img" />
+    <img :src="coverImgUrl" class="bg-img" />
+    <mu-container class="head-msg">
+      <mu-row gutter>
+        <mu-col span="5" class="cover-img">
+          <img :src="coverImgUrl" />
+        </mu-col>
+        <mu-col span="7">
+          <div class="name">{{headMsg.name}}</div>
+          <div class="creator">
+            <img :src="headMsg.avatarUrl" alt="" />
+            <span>{{headMsg.nickname}}</span>
+            <mu-icon
+              value="keyboard_arrow_right"
+              size = "18"
+              class="icon"
+              color = "#fff"></mu-icon>
+          </div>
+        </mu-col>
+      </mu-row>
+      <mu-row gutter style="color:#fff;text-align:center;margin-top:20px">
+        <mu-col v-for="(item,index) in icons" :key="index" span="3">
+          <mu-icon
+            color="#fff"
+            size = "30"
+            :value="item.icon"></mu-icon>
+          <div>{{item.name}}</div>
+        </mu-col>
+      </mu-row>
+    </mu-container>
     <b-scroll
-    :data = "[tracks,headMsg]"
+    :data = "[tracks]"
     :listenScroll = "true"
     @scroll = "scroll"
     class="wrapper">
       <div class="content">
-        <mu-container class="head-msg">
-          <mu-row gutter>
-            <mu-col span="5" class="cover-img">
-              <img v-lazy="coverImgUrl" />
-            </mu-col>
-            <mu-col span="7">
-              <div class="name">{{headMsg.name}}</div>
-              <div class="creator">
-                <img v-lazy="headMsg.avatarUrl" alt="" />
-                <span>{{headMsg.nickname}}</span>
-                <mu-icon
-                value="keyboard_arrow_right"
-                size = "18"
-                class="icon"
-                color = "#fff"></mu-icon>
-              </div>
-            </mu-col>
-          </mu-row>
-          <mu-row gutter style="color:#fff;text-align:center;margin-top:20px">
-            <mu-col v-for="(item,index) in icons" :key="index" span="3">
-              <mu-icon
-              color="#fff"
-              size = "30"
-              :value="item.icon"></mu-icon>
-              <div>{{item.name}}</div>
-            </mu-col>
-          </mu-row>
-        </mu-container>
         <div class="tracks">
           <mu-row class="track-head" :class="{track_head_hold:holdTitle}">
             <mu-col span="1" style="vertical-align:middle;padding:9px 0px">
@@ -58,7 +58,7 @@
               收藏（{{tracks.subscribedCount}}）
             </mu-col>
           </mu-row>
-          <mu-row class="playlist" v-for="(play,index) in tracks.playlist" :key="index">
+          <mu-row class="playlist" align-items="center" v-for="(play,index) in tracks.playlist" :key="index">
             <mu-col class="list-icon" span="1">{{index + 1}}</mu-col>
             <mu-col span="9">
               <div class="music-name">
@@ -99,19 +99,49 @@ export default {
   computed: {
     ...mapGetters([
       'songlist'
-    ])
-  },
-  created(){
-    console.log(this.songlist)
+    ]),
+    headMsg(){
+      let name = this.songlist.playlist.name
+      return {
+        name,
+        description: this.songlist.playlist.description,
+        commenCount: this.songlist.playlist.commenCount,
+        shareCount: this.songlist.playlist.shareCount,
+        userId: this.songlist.playlist.userId,
+        avatarUrl: this.songlist.playlist.creator.avatarUrl,
+        nickname: this.songlist.playlist.creator.nickname,
+        tags: this.songlist.playlist.creator.tags,
+      }
+    },
+    tracks(){
+      return {
+        trackCount: this.songlist.playlist.trackCount,
+        playlist: this.songlist.playlist.tracks,
+        subscribedCount: this.songlist.playlist.subscribedCount,
+      }
+    },
+    coverImgUrl() {
+      return this.songlist.playlist.coverImgUrl
+    }
+    // getDate: function(){
+    //     let dataResult = this.songlist.playlist
+    //     this.coverImgUrl = dataResult.coverImgUrl
+    //     this.headMsg.name = dataResult.name
+    //     this.headMsg.description = dataResult.description
+    //     this.headMsg.commenCount = dataResult.commenCount
+    //     this.headMsg.shareCount = dataResult.shareCount
+    //     this.headMsg.userId = dataResult.userId
+    //     this.headMsg.avatarUrl = dataResult.creator.avatarUrl
+    //     this.headMsg.nickname = dataResult.creator.nickname
+    //     this.headMsg.tags = dataResult.creator.tags
+    //     this.tracks.trackCount = dataResult.trackCount
+    //     this.tracks.playlist = dataResult.tracks
+    //     this.tracks.subscribedCount = dataResult.subscribedCount
+    // }
   },
   data() {
     return {
-        id: this.$route.params.id,
-        // id: 2187262716,
-        headMsg:{},
-        tracks: {},
         holdTitle: false,
-        coverImgUrl: "",
         icons: [
           {
             icon: "chat",
@@ -133,38 +163,15 @@ export default {
         ]
     }
   },
-  mounted: function(){
-      // console.log(this.id)
-      this.getData()
-  },
   methods: {
     back(){
       this.$router.go(-1)
     },
     scroll(pos){
       if (pos.y < -238) {
-        console.log("ooo")
           this.holdTitle = true
       }
     },
-    getData(){
-      api.getSongListDetail(this.id)
-      .then((res) => {
-        let dataResult = res.data.playlist
-        this.coverImgUrl = dataResult.coverImgUrl
-        this.headMsg.name = dataResult.name
-        this.headMsg.description = dataResult.description
-        this.headMsg.commenCount = dataResult.commenCount
-        this.headMsg.shareCount = dataResult.shareCount
-        this.headMsg.userId = dataResult.userId
-        this.headMsg.avatarUrl = dataResult.creator.avatarUrl
-        this.headMsg.nickname = dataResult.creator.nickname
-        this.headMsg.tags = dataResult.creator.tags
-        this.tracks.trackCount = dataResult.trackCount
-        this.tracks.playlist = dataResult.tracks
-        this.tracks.subscribedCount = dataResult.subscribedCount
-      })
-    }
   }
 }
 </script>
@@ -192,41 +199,37 @@ export default {
       max-width: 100%
       max-height: 100%
       -webkit-filter: blur(20px)
+    .head-msg
+      margin-top: 10px
+      .row.funct-icon
+        text-align: center
+        color: $color-text
+      .cover-img
+        img
+          max-width: 100%
+          max-height: 100%
+          border-radius: 5px
+      .name
+        margin-top: 20px
+        color: $color-text
+        font-weight: $font-weight-title
+        font-size: $font-size-small-medium-x
+      .creator
+        margin-top: 20px
+        img
+          width: 30px
+          height: 30px
+          border-radius: 50%
+          vertical-align: middle
+        span
+          color: $color-text
+        .icon
+          vertical-align: middle
     .wrapper
       height: calc(100% - 102px)
       width: 100%
-      overflow: hidden
       .content
-        .head-msg
-          margin-top: 10px
-          .row.funct-icon
-            text-align: center
-            color: $color-text
-          .cover-img
-            img
-              max-width: 100%
-              max-height: 100%
-              border-radius: 5px
-          .name
-            margin-top: 20px
-            color: $color-text
-            font-weight: $font-weight-title
-            font-size: $font-size-small-medium-x
-          .creator
-            margin-top: 20px
-            img
-              width: 30px
-              height: 30px
-              border-radius: 50%
-              vertical-align: middle
-            span
-              color: $color-text
-            .icon
-              vertical-align: middle
         .tracks
-          div.track_head_hold
-            position: sticky
-            top: 46px
           .track-head
             margin-top: 10px
             border-top-left-radius: 10px
@@ -236,7 +239,7 @@ export default {
           .playlist
             padding: 8px 0px
             background-color: $color-background-height
-            border: 1px solid $color-border
+            border-bottom: 1px solid $color-border
             .list-icon
               color: #aaa
               text-align: center
